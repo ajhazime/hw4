d0c9b6b06dd627c9babef12995714e72a3dc28e5
AJ Hazime Jr <ahazime@usc.edu>
2025-11-15 00:44:46 +0000

    Finished equal-paths.cpp

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,9 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <algorithm> 
 #endif
 
 #include "equal-paths.h"
@@ -9,10 +12,45 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool maxDepth(Node* root, int& height){
+	if (root == nullptr){
+		height = 0;
+		return true;
+	}
+
+
+	if( (root->left == nullptr) && (root->right == nullptr) ){
+		height = 1;
+		return true;
+	}
+
+	int leftTH = 0;
+	int rightTH = 0;
+	bool leftBool = 1;
+	bool rightBool = 1;
+
+	if(root->left){
+		leftBool = maxDepth(root->left, leftTH);
+	}
+	if(root->right){
+		rightBool = maxDepth(root->right, rightTH);
+	}
+
+	height = 1 + max(leftTH, rightTH);
+
+	if(root->right == nullptr || root->left == nullptr){
+		return leftBool && rightBool;
+	}
+	return leftBool && rightBool && (leftTH == rightTH);
+}
+
+
+
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+	int height = 0;
+	return maxDepth(root, height);
 
 }
 
